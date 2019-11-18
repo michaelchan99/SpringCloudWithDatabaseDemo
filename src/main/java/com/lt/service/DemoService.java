83143bad0257731125cad3be95658c2a1ea3070c
michael <michaelchan99@gmail.com>
2019-11-19 01:12:40 +0800

    DemoService

---
+++ src/main/java/com/lt/service/DemoService.java
@@ -0,0 +1,11 @@
+package com.lt.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String demoService() {
+        return "Michael";
+    }
+}
